494f8cd898f31d40b75949c28e755af847b85905
jaimemjr <82957830+jaimemjr@users.noreply.github.com>
2021-04-21 23:21:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-function compute() {
+function compute()
+{
     var principal = document.getElementById("principal").value;
-    if (principal <= 0) { // zero or negative
-        alert("Enter with a positive number:");
+
+    if(principal<=0){
+        alert("Enter a positive number");
         document.getElementById("principal").focus();
-    } else {
-        var valueRate = document.getElementById("rate").value;
-        var numberOfYears = document.getElementById("years").value;
-        var valueOfInterest = principal * years * rate / 100;
+    }else{
+
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
         var year = new Date().getFullYear() + parseInt(years);
-        document.getElementById("result").innerHTML = `If you deposit <span class="highlight">${principal}</span>,<br />at an interest rate of <span class="highlight">${rate}%</span>.<br />You will receive an amount of <span class="highlight">${interest}</span>,<br />in the year <span class="highlight">${year}</span>`;
+        
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>at an interest rate of <mark>" + rate +"%</mark><br>You will receive an amount of <mark>" + interest + "</mark>,<br>In the year <mark>" + year + "</mark>";
     }
 }
-function updateRate() {
+
+function updateRate()
+{
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval + "%";
+    document.getElementById("rate_val").innerText=rateval;
 }
-function clearResult() {
-    document.getElementById("result").innerHTML = "";
-}  
